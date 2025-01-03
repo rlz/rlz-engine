c001916e71ab0ad2981ab220f978129db38d808e
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-01-03 14:01:15 +0300

    Add auth logout API call

--- src/client/api/auth.ts
+++ src/client/api/auth.ts
@@ -1,5 +1,7 @@
+import { z } from 'zod'
+
 import { API_AUTH_RESPONSE_SCHEMA_V0, ApiAuthResponseV0, ApiSigninRequestV0, ApiSignupRequestV0 } from '../../shared/api/auth'
-import { apiCall } from './api'
+import { apiCall, AuthParam } from './api'
 
 export async function apiSignup(name: string, email: string, password: string): Promise<ApiAuthResponseV0> {
     const req: ApiSignupRequestV0 = {
@@ -18,4 +20,8 @@ export async function apiSignin(name: string, password: string): Promise<ApiAuth
     }
 
     return apiCall('post', 'signin', null, null, req, API_AUTH_RESPONSE_SCHEMA_V0)
+}
+
+export async function apiLogout(auth: AuthParam): Promise<void> {
+    await apiCall('post', 'logout', auth, null, null, z.undefined())
 }
